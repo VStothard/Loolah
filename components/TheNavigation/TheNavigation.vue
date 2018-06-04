987ebbbf08abd12485e3a4343fb448e73c9e68e6
Verity Stothard <vjstothard@gmail.com>
2018-06-04 22:34:55 +1000

    nuxt set up

---
+++ components/TheNavigation/TheNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <nav class="border border-black">
+            <nuxt-link to="/" tag="li"><a>Home</a></nuxt-link>
+            <nuxt-link to="/about" tag="li"><a>About</a></nuxt-link>
+            <nuxt-link to="/" tag="li"><a>Contact</a></nuxt-link>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        background: grey;
+    }
+</style>
