7c459403d2a2d8f4907940c4f699afeca2095118
Verity Stothard <vjstothard@gmail.com>
2018-06-21 20:18:21 +1000

    form

--- components/TheFooter/TheFooter.vue
+++ components/TheFooter/TheFooter.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <form name="contact" netlify>
+        <form name="contact" method="POST" netlify>
             <p>
-                <label>Name <input type="text" name="name" /></label>
+                <label>Your Name: <input type="text" name="name" /></label>   
             </p>
             <p>
-                <label>Email <input type="email" name="email" /></label>
+                <label>Your Email: <input type="email" name="email" /></label>
             </p>
             <p>
-                <button type="submit">Send ME</button>
+                <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+                </select></label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
             </p>
         </form>
     </div>
